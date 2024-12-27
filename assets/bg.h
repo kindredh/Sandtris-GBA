57574fd55ca3fe495cbe3b9dab28c960f4a8906c
kindredh <47900763+kindredh@users.noreply.github.com>
2024-12-27 19:14:39 +0000

    Add files via upload

---
+++ assets/bg.h
@@ -0,0 +1,22 @@
+/*
+ * Exported with nin10kit v1.7
+ * Time-stamp: Friday 12/27/2024, 19:12:18
+ * 
+ * Image Information
+ * -----------------
+ * C:\Users\kindr\Desktop\bg.png 240@160
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef BG2_H
+#define BG2_H
+
+extern const unsigned short bg[38400];
+#define BG_SIZE 76800
+#define BG_LENGTH 38400
+#define BG_WIDTH 240
+#define BG_HEIGHT 160
+
+#endif
+
